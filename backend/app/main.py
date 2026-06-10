0ecf5005994aee7bfa2d08e3eed7e1419d75e168
drthiagolima <drthiagolimaortop@gmail.com>
2026-06-10 09:20:53 -0300

    Allow production website origins in CORS regex

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app = FastAPI(title=settings.app_name)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.frontend_origins,
+    allow_origin_regex=r"^https?://(localhost(:[0-9]+)?|simplesurgery\.com\.br|www\.simplesurgery\.com\.br)$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
